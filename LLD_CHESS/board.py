516c7c56c4bc706c693abb8f110f39c1055a7e95
nikita-singh-1616 <nikita.singh1713@gmail.com>
2024-07-04 18:08:42 +0530

    rook moves, queen moves,king move, check by pawn

--- LLD_CHESS/board.py
+++ LLD_CHESS/board.py
@@ -31,36 +31,62 @@ class ChessBoard(BoardInterface):
         """pawn initialization"""
         for i in range(8):
             self.board[1][i] = self.black.piece_set['pawns'][i]
+            self.black.piece_set['pawns'][i].position = [1,i]
 
         for i in range(8):
             self.board[6][i] = self.white.piece_set['pawns'][i]
+            self.white.piece_set['pawns'][i].position = [6,i]
+
         """rook initialization"""
         self.board[0][0] = self.black.piece_set['rooks'][0]
+        self.black.piece_set['rooks'][0].position = [0,0]
         self.board[0][self.size - 1] = self.black.piece_set['rooks'][1]
+        self.black.piece_set['rooks'][1].position = [0,self.size - 1]
         self.board[self.size - 1][0] = self.white.piece_set['rooks'][0]
+        self.white.piece_set['rooks'][0].position = [self.size - 1,0]
         self.board[self.size - 1][self.size - 1] = self.white.piece_set['rooks'][1]
+        self.white.piece_set['rooks'][1].position = [self.size - 1,self.size - 1]
+
         """knight initialization"""
         self.board[0][1] = self.black.piece_set['knights'][0]
+        self.black.piece_set['knights'][0].position = [0,1]
         self.board[0][self.size - 2] = self.black.piece_set['knights'][1]
+        self.black.piece_set['knights'][1].position = [0,self.size - 2]
         self.board[self.size - 1][1] = self.white.piece_set['knights'][0]
+        self.white.piece_set['knights'][0].position = [self.size - 1,1]
         self.board[self.size - 1][self.size - 2] = self.white.piece_set['knights'][1]
+        self.white.piece_set['knights'][1].position = [self.size - 1,self.size - 2]
 
         """bishop initialization"""
         self.board[0][2] = self.black.piece_set['bishops'][0]
+        self.black.piece_set['bishops'][0].position = [0,2]
         self.board[0][self.size - 3] = self.black.piece_set['bishops'][1]
+        self.black.piece_set['bishops'][1].position = [0,self.size - 3]
         self.board[self.size - 1][2] = self.white.piece_set['bishops'][0]
+        self.white.piece_set['bishops'][0].position = [self.size - 1,2]
         self.board[self.size - 1][self.size - 3] = self.white.piece_set['bishops'][1]
+        self.white.piece_set['bishops'][1].position = [self.size - 1,self.size - 3]
 
         """king initialization"""
         self.board[0][3] = self.black.piece_set['king']
+        self.black.piece_set['king'].position = [0,3]
         self.board[self.size - 1][3] = self.white.piece_set['king']
+        self.white.piece_set['king'].position = [self.size - 1,3]
 
         """queen initialization"""
         self.board[0][4] = self.black.piece_set['queen']
+        self.black.piece_set['queen'] = [0,4]
         self.board[self.size - 1][4] = self.white.piece_set['queen']
+        self.white.piece_set['queen'] = [self.size - 1,4]
 
     def display(self):
+        print(" ",end=" ")
         for i in range(self.size):
+            print(i,end='  ')
+        print()
+
+        for i in range(self.size):
+            print(i,end=" ")
             for j in range(self.size):
                 if self.board[i][j] != '_':
                     print(self.board[i][j].name, end=" ")
@@ -106,13 +132,30 @@ class ChessBoard(BoardInterface):
                         print('Invalid move')
                         continue
                     else:
+                        prev_state_1 = self.board[x][y]
+                        prev_state_2 = self.board[new_x][new_y]
                         self.move([x,y],[new_x,new_y])
+                        if self.check_for_check(current_player.color):
+                            self.board[x][y] = prev_state_1
+                            self.board[new_x][new_y] = prev_state_2
+                            print('check alert')
+                            continue
                         toggle = not toggle
                         break
 
     def move(self, cur_pos,next_pos):
         self.board[cur_pos[0]][cur_pos[1]],self.board[next_pos[0]][next_pos[1]] = '_',self.board[cur_pos[0]][cur_pos[1]]
+        self.board[next_pos[0]][next_pos[1]].position = [next_pos[0],next_pos[1]]
         self.display()
+
+    def check_for_check(self,cur_color):
+        if self.black.piece_set['king'].check_for_check(self.board):
+            print('CHECK FOR BLACK')
+            return cur_color == self.black.color
+        if self.white.piece_set['king'].check_for_check(self.board):
+            print('CHECK FOR WHITE')
+            return cur_color == self.white.color
+
 
 
 obj = ChessBoard()
--- LLD_CHESS/pieces.py
+++ LLD_CHESS/pieces.py
@@ -54,17 +54,170 @@ class King(Piece):
         super().__init__(f'{color}K')
         self.color = color
 
-    def move(self):
-        pass
+    def move(self, board):
+        x = self.position[0]
+        y = self.position[1]
+        top = [x + 1, y] if x + 1 < len(board) and (
+                board[x + 1][y] == '_' or board[x + 1][y].color != self.color) else []
+        bottom = [x - 1, y] if x - 1 >= 0 and (board[x - 1][y] == '_' or board[x - 1][y].color != self.color) else []
+        right = [x, y + 1] if y + 1 < len(board[0]) and (
+                    board[x][y + 1] == '_' or board[x][y + 1].color != self.color) else []
+        left = [x, y - 1] if y - 1 >= 0 and (board[x][y - 1] == '_' or board[x][y - 1].color != self.color) else []
+        possible_moves = top + left + right + bottom
+        return possible_moves
 
+    def check_for_check(self,board):
+        x = self.position[0]
+        y = self.position[1]
+        """check for pawns"""
+        if self.color == 'W':
+            if ((board[x-1][y+1] != '_' and board[x-1][y+1].color!=self.color and board[x-1][y+1].name[-1] == 'P') or
+                    (board[x-1][y-1] != '_' and board[x-1][y-1].color!=self.color and board[x-1][y-1].name[-1] == 'P')):
+                return True
+        if self.color == 'B':
+            if ((board[x+1][y+1] != '_' and board[x+1][y+1].color!=self.color and board[x + 1][y + 1].name[-1] == 'P')
+                    or (board[x+1][y-1] != '_' and board[x+1][y-1].color!=self.color) and board[x+1][y-1].name[-1]=='P'):
+                return True
+        """check for rook"""
+        top_x = x - 1
+        bot_x = x + 1
+        while top_x >= 0:
+            if board[top_x][y] == '_':
+                top_x -= 1
+            else:
+                if board[top_x][y].color != self.color:
+                    if board[top_x][y].name[-1] == 'R' or board[top_x][y].name[-1] == 'Q':
+                        return True
+                break
+
+        while bot_x < len(board):
+            if board[bot_x][y] == '_':
+                bot_x += 1
+            else:
+                if board[bot_x][y].color != self.color:
+                    if  board[bot_x][y].name[-1] == 'R' or board[bot_x][y].name[-1] == 'Q':
+
+                        return True
+                break
+        #             horizontal movement
+        right_y = y + 1
+        left_y = y - 1
+        while right_y < len(board[0]):
+            if board[x][right_y] == '_':
+                right_y += 1
+            else:
+                if board[x][right_y].color != self.color:
+                    if board[x][right_y].name[-1] == 'R' or board[x][right_y].name[-1] == 'Q':
+                        return True
+                break
+
+        while left_y >= 0:
+            if board[x][left_y] == '_':
+                left_y -= 1
+            else:
+                if board[x][left_y].color != self.color:
+                    if board[x][left_y].name[-1] == 'R' or board[x][left_y].name[-1] == 'Q':
+                        return True
+                break
+        moves = [(-1, -2), (-2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2)]
+        for move in moves:
+            x = self.position[0]
+            y = self.position[1]
+            new_x = x + move[0]
+            new_y = y + move[1]
+            if 0 <= new_x < len(board) and 0 <= new_y < len(board[0]):
+                if board[new_x][new_y] != '_' and board[new_x][new_y].color != self.color:
+                    if board[new_x][new_y].name[-1] == 'N':
+                        return True
+        return False
 
 class Queen(Piece):
     def __init__(self, color):
         super().__init__(f'{color}Q')
         self.color = color
 
-    def move(self):
-        pass
+    def move(self, board):
+        possible_moves = []
+        n = len(board)
+        cur_x = self.position[0]
+        cur_y = self.position[1]
+        #         vertical movement
+        top_x = cur_x - 1
+        bot_x = cur_x + 1
+        while top_x >= 0:
+            if board[top_x][cur_y] == '_':
+                possible_moves.append([top_x, cur_y])
+                top_x -= 1
+            else:
+                if board[top_x][cur_y].color != self.color:
+                    possible_moves.append([top_x, cur_y])
+                break
+
+        while bot_x < len(board):
+            if board[bot_x][cur_y] == '_':
+                possible_moves.append([bot_x, cur_y])
+                bot_x += 1
+            else:
+                if board[bot_x][cur_y].color != self.color:
+                    possible_moves.append([bot_x, cur_y])
+                break
+        #             horizontal movement
+        right_y = cur_y + 1
+        left_y = cur_y - 1
+        while right_y < len(board[0]):
+            if board[cur_x][right_y] == '_':
+                possible_moves.append([cur_x, right_y])
+                right_y += 1
+            else:
+                if board[cur_x][right_y].color != self.color:
+                    possible_moves.append([cur_x, right_y])
+                break
+
+        while left_y >= 0:
+            if board[cur_x][left_y] == '_':
+                possible_moves.append([cur_x, left_y])
+                left_y -= 1
+            else:
+                if board[cur_x][left_y].color != self.color:
+                    possible_moves.append([cur_x, left_y])
+                break
+
+        i = self.position[0] - 1
+        j = self.position[1] - 1
+        while 0 <= i < n and 0 <= j < n and (board[i][j] == '_' or board[i][j].color != self.color):
+            possible_moves.append([i, j])
+            if board[i][j] != '_':
+                break
+            j -= 1
+            i -= 1
+        # right top diagonal
+        i = self.position[0] - 1
+        j = self.position[1] + 1
+        while 0 <= i < n and 0 <= j < n and (board[i][j] == '_' or board[i][j].color != self.color):
+            possible_moves.append([i, j])
+            if board[i][j] != '_':
+                break
+            j += 1
+            i -= 1
+        # left top diagonal
+        i = self.position[0] + 1
+        j = self.position[1] - 1
+        while 0 <= i < n and 0 <= j < n and (board[i][j] == '_' or board[i][j].color != self.color):
+            possible_moves.append([i, j])
+            if board[i][j] != '_':
+                break
+            j -= 1
+            i += 1
+        # left top diagonal
+        i = self.position[0] + 1
+        j = self.position[1] + 1
+        while 0 <= i < n and 0 <= j < n and (board[i][j] == '_' or board[i][j].color != self.color):
+            possible_moves.append([i, j])
+            if board[i][j] != '_':
+                break
+            j += 1
+            i += 1
+        return possible_moves
 
 
 class Rook(Piece):
@@ -72,8 +225,50 @@ class Rook(Piece):
         super().__init__(f'{color}R')
         self.color = color
 
-    def move(self):
-        pass
+    def move(self, board):
+        possible_moves = []
+        cur_x = self.position[0]
+        cur_y = self.position[1]
+        #         vertical movement
+        top_x = cur_x - 1
+        bot_x = cur_x + 1
+        while top_x >= 0:
+            if board[top_x][cur_y] == '_':
+                possible_moves.append([top_x, cur_y])
+                top_x -= 1
+            else:
+                if board[top_x][cur_y].color != self.color:
+                    possible_moves.append([top_x, cur_y])
+                break
+
+        while bot_x < len(board):
+            if board[bot_x][cur_y] == '_':
+                possible_moves.append([bot_x, cur_y])
+                bot_x += 1
+            else:
+                if board[bot_x][cur_y].color != self.color:
+                    possible_moves.append([bot_x, cur_y])
+                break
+        #             horizontal movement
+        right_y = cur_y + 1
+        left_y = cur_y - 1
+        while right_y < len(board[0]):
+            if board[cur_x][right_y] == '_':
+                possible_moves.append([cur_x, right_y])
+                right_y += 1
+            else:
+                if board[cur_x][right_y].color != self.color:
+                    possible_moves.append([cur_x, right_y])
+                break
+        while left_y >= 0:
+            if board[cur_x][left_y] == '_':
+                possible_moves.append([cur_x, left_y])
+                left_y -= 1
+            else:
+                if board[cur_x][left_y].color != self.color:
+                    possible_moves.append([cur_x, left_y])
+                break
+        return possible_moves
 
 
 class Bishop(Piece):
@@ -81,50 +276,46 @@ class Bishop(Piece):
         super().__init__(f'{color}B')
         self.color = color
 
-    def move(self,board):
+    def move(self, board):
         n = len(board)
         possible_moves = []
         # left top diagonal
-        i = self.position[0]-1
-        j = self.position[1]-1
-        while i >= 0 and j>=0 and i<n and j<n and (board[i][j]=='_' or board[i][j].color!=self.color):
+        i = self.position[0] - 1
+        j = self.position[1] - 1
+        while 0 <= i < n and 0 <= j < n and (board[i][j] == '_' or board[i][j].color != self.color):
             possible_moves.append([i, j])
-            if board[i][j]!='_':
+            if board[i][j] != '_':
                 break
-            j-=1
-            i-=1
+            j -= 1
+            i -= 1
         # right top diagonal
-        i = self.position[0]-1
-        j = self.position[1]+1
-        while i >= 0 and j >= 0 and i < n and j < n  and (board[i][j]=='_' or board[i][j].color!=self.color):
+        i = self.position[0] - 1
+        j = self.position[1] + 1
+        while 0 <= i < n and 0 <= j < n and (board[i][j] == '_' or board[i][j].color != self.color):
             possible_moves.append([i, j])
-            if board[i][j]!='_':
+            if board[i][j] != '_':
                 break
             j += 1
             i -= 1
         # left top diagonal
-        i = self.position[0]+1
-        j = self.position[1]-1
-        while i >= 0 and j >= 0 and i < n and j < n and (board[i][j]=='_' or board[i][j].color!=self.color):
+        i = self.position[0] + 1
+        j = self.position[1] - 1
+        while 0 <= i < n and 0 <= j < n and (board[i][j] == '_' or board[i][j].color != self.color):
             possible_moves.append([i, j])
-            if board[i][j]!='_':
+            if board[i][j] != '_':
                 break
             j -= 1
             i += 1
         # left top diagonal
-        i = self.position[0]+1
-        j = self.position[1]+1
-        while i >= 0 and j >= 0 and i < n and j < n and (board[i][j]=='_' or board[i][j].color!=self.color):
+        i = self.position[0] + 1
+        j = self.position[1] + 1
+        while 0 <= i < n and 0 <= j < n and (board[i][j] == '_' or board[i][j].color != self.color):
             possible_moves.append([i, j])
-            if board[i][j]!='_':
+            if board[i][j] != '_':
                 break
             j += 1
             i += 1
         return possible_moves
-
-
-
-
 
 
 class Knight(Piece):
@@ -140,7 +331,7 @@ class Knight(Piece):
             y = self.position[1]
             new_x = x + move[0]
             new_y = y + move[1]
-            if new_x >= 0 and new_x < len(board) and new_y >= 0 and new_y < len(board[0]):
+            if 0 <= new_x < len(board) and 0 <= new_y < len(board[0]):
                 if board[new_x][new_y] == '_' or board[new_x][new_y].color != self.color:
                     possible_moves.append([new_x, new_y])
         return possible_moves
